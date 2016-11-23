35c943ffe44770733c35fa176da09f2cca6bfda0
pook <pooktomza@hotmail.com>
2016-11-23 15:51:15 +0700

    Get date finish

--- app/src/main/java/rtc/kanchana/sirirat/pookalert/SetupTime.java
+++ app/src/main/java/rtc/kanchana/sirirat/pookalert/SetupTime.java
@@ -2,12 +2,33 @@ package rtc.kanchana.sirirat.pookalert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class SetupTime extends AppCompatActivity {
+    //Explicit
+    private CalendarView calendarView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_time);
-    }
-}
+        //Bind Widget
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        //Calendarview Controller
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+
+                Log.d("pookV1","date ==> " + day + "/" + (month + 1) + "/" + year);
+
+
+            }   //onSelected
+        });
+
+
+    }   // Main Method
+
+}   // Main Class
